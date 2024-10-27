5ee325c2c2608e0d120f98665c8096e7ba0c6934
hasby-umutoniwabo <h.umutoniwa@alustudent.com>
2024-10-27 15:15:03 -0700

    py

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+import sys
+
+def main():
+    # Check the number of arguments
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+
+    # Dictionary mapping operators to functions
+    operations = {
+        '+': add,
+        '-': sub,
+        '*': mul,
+        '/': div
+    }
+
+    # Check if the operator is valid
+    if operator not in operations:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    # Perform the operation
+    result = operations[operator](a, b)
+    print(f"{a} {operator} {b} = {result}")
+
+if __name__ == "__main__":
+    main()
