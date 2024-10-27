dadb1cde0597a5e5728447b78e88a9d031686094
hasby-umutoniwabo <h.umutoniwa@alustudent.com>
2024-10-27 15:36:49 -0700

    py

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,36 +1,38 @@
 #!/usr/bin/python3
-from calculator_1 import add
-from calculator_1 import sub
-from calculator_1 import mul
-from calculator_1 import div
+import calculator_1
 import sys
 
 def main():
-    # Check the number of arguments
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
+        return 1
 
-    a = int(sys.argv[1])
-    operator = sys.argv[2]
-    b = int(sys.argv[3])
+    try:
+        a = int(sys.argv[1])
+        operator = sys.argv[2]
+        b = int(sys.argv[3])
+    except ValueError:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        return 1
 
-    # Dictionary mapping operators to functions
-    operations = {
-        '+': add,
-        '-': sub,
-        '*': mul,
-        '/': div
-    }
-
-    # Check if the operator is valid
-    if operator not in operations:
+    if operator == '+':
+        result = calculator_1.add(a, b)
+    elif operator == '-':
+        result = calculator_1.sub(a, b)
+    elif operator == '*':
+        result = calculator_1.mul(a, b)
+    elif operator == '/':
+        if b == 0:
+            print("Error: Division by zero")
+            return 1
+        result = calculator_1.div(a, b)
+    else:
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+        return 1
 
-    # Perform the operation
-    result = operations[operator](a, b)
     print(f"{a} {operator} {b} = {result}")
+    return 0
 
 if __name__ == "__main__":
-    main()
+    exit(main())
+
