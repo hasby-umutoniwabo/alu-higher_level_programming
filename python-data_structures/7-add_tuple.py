56e33c4513ddfdcd6ee2679aacda23ebcedcf6f7
hasby-umutoniwabo <h.umutoniwa@alustudent.com>
2024-10-27 16:22:16 -0700

    py

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -15,4 +15,3 @@ def add_tuple(tuple_a=(), tuple_b=()):
     first_element = tuple_a[0] + tuple_b[0]
     second_element = tuple_a[1] + tuple_b[1]
     return tuple((first_element, second_element))
-
