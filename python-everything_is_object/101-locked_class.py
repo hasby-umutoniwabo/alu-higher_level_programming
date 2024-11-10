1a6d8ceeddaa6683dbf1d3c0f782635f180e7ecc
hasby-umutoniwabo <h.umutoniwa@alustudent.com>
2024-11-10 05:10:46 -0800

    py

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+"""
+This module defines a LockedClass with restricted attribute setting.
+"""
+
 class LockedClass:
+    """
+    A class that only allows the 'first_name' attribute to be set dynamically.
+    Attempting to set any other attribute raises an AttributeError.
+    """
     __slots__ = ['first_name']
