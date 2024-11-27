be5367bd401b84b1d5d8ec05ec60d34ef3aac1b1
hasby-umutoniwabo <h.umutoniwa@alustudent.com>
2024-11-27 13:51:19 +0000

    get

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# This script sends a GET request to the URL provided as an argument and displays the body of the response only if the status code is 200.
 response=$(curl -s -w "%{http_code}" -o response.txt "$1")
 if [ "$response" -eq 200 ]; then
     cat response.txt
