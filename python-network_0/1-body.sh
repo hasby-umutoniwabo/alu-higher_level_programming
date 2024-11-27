e6577d8f75dc75aef78a49f4b4ed9268b04de61f
hasby-umutoniwabo <h.umutoniwa@alustudent.com>
2024-11-27 13:53:50 +0000

    get

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# sends GET request to the URL provided as an arg displays body if 200
 response=$(curl -s -w "%{http_code}" -o response.txt "$1")
 if [ "$response" -eq 200 ]; then
     cat response.txt
