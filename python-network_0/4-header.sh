dbf362c6a8d0dc2050a4e20561b1efd0a609e946
hasby-umutoniwabo <h.umutoniwa@alustudent.com>
2024-11-29 11:09:33 -0800

    py

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
-<<<<<<< HEAD
-
-# Check if URL is provided
-if [ $# -eq 0 ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# URL argument
-URL=$1
-
-# Send GET request with header and display the response body
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$URL"
-=======
-# This script sends a GET request to the URL with a custom header and displays the body of the response.
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
->>>>>>> e716daa23763625784733288f27e8eb5843d8275
+# display the body of a url call 
+curl -sG "$1" -H "X-School-User-Id: 98"
